4e9b52b25de167c50dac097d32a2951a51e485f9
logan <141536342+loganofc@users.noreply.github.com>
2023-11-15 16:52:56 -0600

    noti.js

---
+++ noti.js
@@ -0,0 +1,8 @@
+function getMessage() {
+  return "Have fun and dont get caught :)";
+}
+(function (_0x39c2f3, _0x6523f) {
+  _0x39c2f3.onload = function () {
+    alert("Have fun and dont get caught :)");
+  };
+})(window, document);
